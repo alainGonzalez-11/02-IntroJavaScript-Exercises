b84fc8256563d1a8f67cd6c7556e6073bf9ba9f6
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2024-05-02 17:20:56 -0600

    Creation of exercise 3 (while) task 5 - Days of the week

--- 03 While/script.js
+++ 03 While/script.js
@@ -9,7 +9,7 @@ function menudeEjercicios() {
   } else if (respuesta == "4") {
     memoria();
   } else if (respuesta == "5") {
-    alert("Función no soportada");
+    evaluaDiasSemana();
   } else {
     alert("Perdón, no entendí tu respuesta");
     menudeEjercicios();
@@ -84,19 +84,56 @@ function memoria() {
 // TAREA 4
 // 4. Crea un programa que solicite al usuario letras o palabras, si es así guardar el resultado. Para terminar de capturar el usuario no debe escribir valor alguno. Al terminar de capturar, mostrar en pantalla la concatenación de todas las palabras capturadas.
 function memoria() {
-    let continuar = true;
-    let frase = '';
-    while (continuar) {
-      let palabra = prompt("Ingresa una palabra (Da clic en ok sin escribir nada para terminar)");
-      if (palabra !== '') {
-        frase += ' ' + palabra;
+  let continuar = true;
+  let frase = "";
+  while (continuar) {
+    let palabra = prompt(
+      "Ingresa una palabra (Da clic en ok sin escribir nada para terminar)"
+    );
+    if (palabra !== "") {
+      frase += " " + palabra;
     } else {
-        continuar = false;
-      }
+      continuar = false;
     }
-    console.log(frase);
   }
-  
+  console.log(frase);
+}
 
-  // TAREA 5
-  // 5. Crea un programa que solicite al usuario un día de la semana (ej: lunes, jueves, domingo, etc). El programa mostrará un mensaje personalizado para cada día de la semana por medio de un alert. Y seguirá pidiendo al usuario introducir otro día. En caso de que el día introducido sea domingo mostrar al usuario el mensaje “Ve a descansar” y terminar la captura de información.
+// TAREA 5
+// 5. Crea un programa que solicite al usuario un día de la semana (ej: lunes, jueves, domingo, etc). El programa mostrará un mensaje personalizado para cada día de la semana por medio de un alert. Y seguirá pidiendo al usuario introducir otro día. En caso de que el día introducido sea domingo mostrar al usuario el mensaje “Ve a descansar” y terminar la captura de información.
+
+function evaluaDiasSemana() {
+  let continuar = true;
+  while (continuar) {
+    let dia = prompt("Ingresa un día de la semana");
+    switch (dia.toLowerCase()) {
+      case "lunes":
+        alert("Feliz inicio de semana");
+        break;
+      case "viernes":
+        alert("Ya se acabó la semana");
+        break;
+      case "sábado":
+      case "sabado":
+        alert("Disfruta tu fin de semana");
+        break;
+      case "domingo":
+        alert("Ve a descansar");
+        continuar = false;
+        break;
+      case "martes":
+        alert("Aprovecha el día");
+        break;
+      case "miercoles":
+      case "miércoles":
+        alert("Estamos a media semana");
+        break;
+      case "jueves":
+        alert("Otro día, otro dolar");
+        break;
+      default:
+        alert("No ingresaste un día de la semana");
+        break;
+    }
+  }
+}
