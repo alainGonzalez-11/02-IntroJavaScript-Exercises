e24bc36048da9178627c71e23e4c7b6f553d5f04
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2024-05-02 16:43:38 -0600

    Creation of exercise 3 (while) task 2 - Loteria

--- 03 While/script.js
+++ 03 While/script.js
@@ -3,7 +3,7 @@ function menudeEjercicios() {
   if (respuesta == "1") {
     multiplosDeCinco();
   } else if (respuesta == "2") {
-    alert("Función no soportada");
+    loteria();
   } else if (respuesta == "3") {
     alert("Función no soportada");
   } else if (respuesta == "4") {
@@ -18,15 +18,45 @@ function menudeEjercicios() {
 
 menudeEjercicios();
 
-
 // TAREA 1
 // 1. Crea un programa que pregunte al usuario un número. Mostrar los números que son múltiplos de 5 desde 1 hasta el número introducido por el usuario.
 
 function multiplosDeCinco() {
-    let limite = prompt("Ingresa un número");
-    index=5;
-    while (index<=limite) {
-        console.log(index);
-        index = index + 5;
+  let limite = prompt("Ingresa un número");
+  index = 5;
+  while (index <= limite) {
+    console.log(index);
+    index = index + 5;
+  }
+}
+
+// TAREA 2
+// 2. Crea un programa que solicite al usuario 2 números entre 1 y 50. Posteriormente mostrar en consola los números del 1 hasta el 50, pero añadir el mensaje “¡Lotería!” solo al mostrar los números indicados por el usuario.
+function loteria() {
+  let respuesta = prompt(
+    "Ingresa dos numeros separados por comas entre el 1 y el 50"
+  );
+  let numeros = respuesta.split(",");
+  if (numeros.length !== 2) {
+    alert("Lo lamento, necesitamos 2 números");
+    loteria();
+    return false;
+  }
+  for (let key in numeros) {
+    if (numeros[key] > 50 || numeros[key] < 1) {
+      alert("Lo lamento, los números no están en el rango");
+      loteria();
+      return false;
     }
+  }
+
+  index = 1;
+  while (index <= 50) {
+    if (index == numeros[0] || index == numeros[1]) {
+      console.log(index + " ¡¡¡Loteria!!!");
+    } else {
+      console.log(index);
+    }
+    index++;
+  }
 }
