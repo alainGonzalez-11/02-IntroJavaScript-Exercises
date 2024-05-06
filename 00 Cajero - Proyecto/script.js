bf417d7b481ed782b96d2760b62f98f6b2ff3dad
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2024-05-06 15:40:33 -0600

    Included initial keyboard configuration

--- 00 Cajero - Proyecto/script.js
+++ 00 Cajero - Proyecto/script.js
@@ -32,53 +32,78 @@ class Cliente {
 }
 
 function newTransaction() {
-   let mainScreen = document.getElementById('Screen');
+  let mainScreen = document.getElementById("Screen");
 
   let user = selectUser();
   setInitialScreen(mainScreen);
 }
 
 function selectUser() {
-  let nombresUsuario = '';
-  usuarios.forEach(usuario => {
+  let nombresUsuario = "";
+  usuarios.forEach((usuario) => {
     console.log(usuario.getNombre());
 
-    nombresUsuario += ' ' + usuario.getNombre();
+    nombresUsuario += " " + usuario.getNombre();
   });
-  let userName = prompt('Ingresa tu usuario, los registrados son' + nombresUsuario);
-  usuarios.forEach(usuario => {
-    
-    if (usuario.getNombre().toLowerCase() == userName.toLowerCase()){
-        console.log('success');
-        return usuario;
+  //let userName = prompt('Ingresa tu usuario, los registrados son' + nombresUsuario);
+  userName = "juan";
+  usuarios.forEach((usuario) => {
+    if (usuario.getNombre().toLowerCase() == userName.toLowerCase()) {
+      console.log("success");
+      return usuario;
     }
   });
-}   
+}
 
-function setInitialScreen(screen){
-    let message = document.createElement('p');
-    message.innerHTML = 'Ingresa tu tarjeta para iniciar';
-    screen.appendChild(message);
-    let card = document.getElementById('CardSlot');
-    card.setAttribute('src', "images/cardSlotOn.svg")
-    card.setAttribute('class', 'CardSlotOn');
-    card.addEventListener('click', insertCard);
+function setInitialScreen(screen) {
+  activateKeyboard();
+  let message = document.createElement("p");
+  message.innerHTML = "Ingresa tu tarjeta para iniciar";
+  screen.appendChild(message);
+  let card = document.getElementById("CardSlot");
+  card.setAttribute("src", "images/cardSlotOn.svg");
+  card.setAttribute("class", "CardSlotOn");
+  card.addEventListener("click", insertCard);
+  
+}
+
+function activateKeyboard() {
+    let buttons = document.getElementsByClassName('keyButton');
+    for (let i = 0; i < buttons.length; i++) {
+        console.log(buttons[i]);
+        buttons[i].addEventListener("click", keyClick);
+    }
+}
+
+function keyClick() {
+    console.log("pressed");
 }
 
 function insertCard() {
-    let card = document.getElementById('CardSlot');
-    card.setAttribute('src', "images/cardSlotOff.svg")
-    card.setAttribute('class', 'CardSlotOff');
-    let mainScreen = document.getElementById('Screen');
+  if (transactionStatus == "Start") {
+    let card = document.getElementById("CardSlot");
+    card.setAttribute("src", "images/cardSlotOff.svg");
+    card.setAttribute("class", "CardSlotOff");
+    let mainScreen = document.getElementById("Screen");
     while (mainScreen.firstChild) {
-        mainScreen.removeChild(mainScreen.firstChild);
+      mainScreen.removeChild(mainScreen.firstChild);
     }
-    
+    mainScreen.setAttribute("class", "ScreenMenu");
+    let instruction = document.createElement("p");
+    instruction.innerHTML = "Ingresa tu NIP";
+    instruction.style.fontSize = "2em";
+    instruction.style.fontWeight = "bold";
+    instruction.style.gridColumn = "2";
+    instruction.style.gridRow = "2";
+    mainScreen.appendChild(instruction);
+    transactionStatus = "Pin";
+  } else {
+  }
 }
 
-
 let usuarios = [
-    new Cliente("Juan", "Rulfo", 600, 1234),
-    new Cliente("Pedro", "Paramo", 150, 5678)
-  ];
+  new Cliente("Juan", "Rulfo", 600, 1234),
+  new Cliente("Pedro", "Paramo", 150, 5678),
+];
+transactionStatus = "Start";
 newTransaction();
