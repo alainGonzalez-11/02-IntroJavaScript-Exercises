79c625a97814dca3cf2eaea813fc1aca5ea30e41
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2024-05-09 09:58:17 -0600

    Created function for exercise 4 task 2 Listing Pokemon

--- 04 For/script.js
+++ 04 For/script.js
@@ -18,7 +18,7 @@ menudeEjercicios();
 // 1. Crea un programa que imprima en consola los números impares del 1 al 50.
 
 function oddNumbers() {
-  for (let i = 1; i <50 ; i++) {
+  for (let i = 1; i < 50; i++) {
     if (i % 2 !== 0) {
       console.log(i);
     }
@@ -29,7 +29,56 @@ function oddNumbers() {
 // 2. Crea un programa que pregunte al usuario un número. Usando el archivo de arreglo de Pokémons, mostrar solo los nombres de los Pokémons cuyos números que son múltiplos de 5 desde 1 hasta el número introducido por el usuario. https://pastebin.com/Zzk8g7Z6.
 
 function pokemon() {
-  // TODO: Implement function
+  var pokemons = [
+    "bulbasaur",    "ivysaur",    "venusaur",
+    "charmander",    "charmeleon",    "charizard",
+    "squirtle",    "wartortle",    "blastoise",
+    "caterpie",    "metapod",    "butterfree",
+    "weedle",    "kakuna",
+    "beedrill",
+    "pidgey",    "pidgeotto",    "pidgeot",
+    "rattata",    "raticate",
+    "spearow",    "fearow",
+    "ekans",    "arbok",
+    "pikachu",    "raichu",
+    "sandshrew",    "sandslash",
+    "nidoran-f",    "nidorina",    "nidoqueen",    "nidoran-m",    "nidorino",    "nidoking",
+    "clefairy",    "clefable",
+    "vulpix",    "ninetales",
+    "jigglypuff",    "wigglytuff",
+    "zubat",    "golbat",
+    "oddish",    "gloom",    "vileplume",
+    "paras",    "parasect",
+    "venonat",    "venomoth",
+    "diglett",    "dugtrio",
+    "meowth",    "persian",
+    "psyduck",    "golduck",
+    "mankey",    "primeape",
+    "growlithe",    "arcanine",
+    "poliwag",    "poliwhirl",    "poliwrath",
+    "abra",    "kadabra",    "alakazam",
+    "machop",    "machoke",    "machamp",
+    "bellsprout",    "weepinbell",    "victreebel",
+    "tentacool",    "tentacruel",
+    "geodude",    "graveler",    "golem",
+    "ponyta",    "rapidash",
+    "slowpoke",    "slowbro",
+    "magnemite",    "magneton",
+    "farfetchd",
+    "doduo",    "dodrio",
+    "seel",    "dewgong",
+    "grimer",    "muk",
+    "shellder",    "cloyster",
+    "gastly",    "haunter",    "gengar",
+    "onix",
+    "drowzee",    "hypno",
+    "krabby",    "kingler",
+    "voltorb",
+  ];
+
+  for (var i = 1; i < pokemons.length; i++) {
+    if (i%5==0) console.log(pokemons[i]);
+  }
 }
 
 // TAREA 3
