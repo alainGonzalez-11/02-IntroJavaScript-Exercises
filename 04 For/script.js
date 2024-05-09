c87d0cfd63a0b972bdec5d2a699e155b7bab4368
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2024-05-09 08:29:17 -0600

    'Created structure of exercise 04 for

---
+++ 04 For/script.js
@@ -0,0 +1,36 @@
+function menudeEjercicios() {
+  let respuesta = prompt("Indica que ejercicio quieres (1-3)");
+  if (respuesta == "1") {
+    oddNumbers();
+  } else if (respuesta == "2") {
+    pokemon();
+  } else if (respuesta == "3") {
+    checkNumbers();
+  } else {
+    alert("Perdón, no entendí tu respuesta");
+    menudeEjercicios();
+  }
+}
+
+menudeEjercicios();
+
+// TAREA 1
+// 1. Crea un programa que imprima en consola los números impares del 1 al 50.
+
+function oddNumbers() {
+  // TODO: Implement function
+}
+
+// TAREA 2
+// 2. Crea un programa que pregunte al usuario un número. Usando el archivo de arreglo de Pokémons, mostrar solo los nombres de los Pokémons cuyos números que son múltiplos de 5 desde 1 hasta el número introducido por el usuario. https://pastebin.com/Zzk8g7Z6.
+
+function pokemon() {
+  // TODO: Implement function
+}
+
+// TAREA 3
+// 3. Crea un programa que recorra el arreglo [4,”dos”,8,”tres”,5,9,1,”cero”] y muestre en consola solo los elementos que son tipo número.
+
+function checkNumbers() {
+  // TODO: Implement function
+}
